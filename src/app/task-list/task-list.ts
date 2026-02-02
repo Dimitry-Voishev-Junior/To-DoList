eb315f470c088344c05c709a20f79a42e24f2fab
voishev <dimitryjunior.dev@gmail.com>
2026-02-02 14:44:25 -0300

    Criando HTML dos componentes

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskFilter } from "../task-filter/task-filter";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [TaskFilter, RouterLink],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
